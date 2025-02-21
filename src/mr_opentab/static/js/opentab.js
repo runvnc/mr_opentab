83228cb989850e22123ec7e0f7c1d425145dee00
Jason Livesay <runvnc@gmail.com>
2025-02-21 09:05:29 -0600

    update

--- src/mr_opentab/static/js/opentab.js
+++ src/mr_opentab/static/js/opentab.js
@@ -2,7 +2,17 @@
 function doNav(URL) {
   let win = window.open(URL, '_blank');
   if (!win) {
-    window.location.href = URL
+    console.log('Popup blocked');
+    const randomID = Math.random().toString(36).substring(7);
+    dialog_html = `
+      <dialog id="dialog${randomID}">
+        <h1>Open in new tab</h1>
+        <p>Click the link below to open the link in a new tab:</p>
+        <a href="${URL}">${URL}</button>
+      </dialog>`
+    document.body.insertAdjacentHTML('beforeend', dialog_html);
+    dialog = document.getElementById(`dialog${randomID}`);
+    dialog.showModal();
   }
 }
 
